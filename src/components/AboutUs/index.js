85a16c3a1d1e32200c4a8d0d43950a10eed19799
mirbekmuratov <mirbekmuratov@MacBook-Air-Mirbek.local>
2020-10-10 01:54:41 +0600

    fix bug carousel

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import Carousel from '../Carousel'
+import React, { Component } from "react";
+import Carousel from "../Carousel";
 
 class AboutUs extends Component {
   render() {
@@ -10,5 +10,7 @@ class AboutUs extends Component {
     );
   }
 }
+
+// test
 
 export default AboutUs;
